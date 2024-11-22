0611109b6dcf00a5c99893861aac15e3a0eae704
nicomigu <nicoefrmiguel@gmail.com>
2024-11-22 22:22:48 +0800

    added naming server

--- backend/shopping-cart-service/src/main/java/com/nicomigu/shopping_cart_service/ShoppingCartServiceApplication.java
+++ backend/shopping-cart-service/src/main/java/com/nicomigu/shopping_cart_service/ShoppingCartServiceApplication.java
@@ -2,8 +2,12 @@ package com.nicomigu.shopping_cart_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class ShoppingCartServiceApplication {
 
 	public static void main(String[] args) {
